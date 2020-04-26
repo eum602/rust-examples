f0e54ea9eb4e5425a6bfd178a02316bcaa1c3755
Erick <eum602@gmail.com>
2020-04-26 11:55:03 -0500

    adds example to read line, parse string to int, comparte, reassing a variable to change type(shadow),and the use of loops

--- b_guess/src/main.rs
+++ b_guess/src/main.rs
@@ -1,15 +1,37 @@
 use std::io; //accept, amongst others, user input
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1, 101);//random from 1 to 100, defaults type to int32
+    println!("The secret number is: {}", secret_number);
 
-    let mut guess = String::new();//let allows to create a variable, "mut" ==> MUTABLE variable, otherwise by default is immutable
-    //"::" indicates new is a method from string
+    loop {
+        println!("
+        Please input your guess.");
 
-    io::stdin().read_line(&mut guess)//passing what is read to a mutable reference
-        .expect("Failed to read line");//handling error by taking the result
+        let mut guess = String::new();//let allows to create a variable, "mut" ==> MUTABLE variable, otherwise by default is immutable
+        //"::" indicates new is a method from string.Rust assumes 'guess' is a string ==> type inference
 
-    println!("You guessed: {}", guess);//{} is a placeholder
+        io::stdin().read_line(&mut guess)//passing what is read to a mutable reference
+            .expect("Failed to read line");//handling error by taking the result
+
+        let guess: u32 = guess.trim().parse() //SHADOWING the previous value of 'guess' ==> converts from one to another type.
+        //trim method ==> eliminates spaces at the beginning and at the end of guess. eg. 5\n ==> \n because of pressing enter.
+        //parse method ==> converts string to the specified number type (in this case to u32)
+        .expect("Please enter a number");
+
+        println!("You guessed: {}", guess);//{} is a placeholder
+
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win");
+                break; //breaking the loop
+            }
+        }        
+    }    
 }
